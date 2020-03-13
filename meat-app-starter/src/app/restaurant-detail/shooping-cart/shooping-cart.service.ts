7128521df3d1084bc8cc9ee9937cc3b42e31f9d6
Manoel <manoel.junior.ext@sascar.com>
2020-03-13 17:05:50 -0300

    Criando serviço.

---
+++ meat-app-starter/src/app/restaurant-detail/shooping-cart/shooping-cart.service.ts
@@ -0,0 +1,19 @@
+export class ShoopingCartService {
+    items: any[]
+
+    clear(){
+
+    }
+
+    addItem(item : any) {
+    
+    }
+
+    removeItem(item: any){
+
+    }
+
+    total(): number {
+        return 0;
+    }
+}
