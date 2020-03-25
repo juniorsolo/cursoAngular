b786ebab9d5f56466288e2160af2a96e3e924728
Manoel <manoel.junior.ext@sascar.com>
2020-03-25 11:10:56 -0300

    Criando componente para ordenar itens.

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ShoopingCartService } from './restaurant-detail/shooping-cart/shooping-
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
 
 
 
@@ -40,7 +41,8 @@ import { RadioComponent } from './shared/radio/radio.component';
     ReviewsComponent,
     OrderComponent,
     InputComponent,
-    RadioComponent
+    RadioComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
