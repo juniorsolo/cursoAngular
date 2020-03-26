193adfd5bcca27cb447497d1714aceb41c2b1a6b
Manoel <manoel.junior.ext@sascar.com>
2020-03-26 18:06:16 -0300

    Criando serviço para ordem de compras.

---
+++ meat-app-starter/src/app/order/order.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { ShoopingCartService } from "app/restaurant-detail/shooping-cart/shooping-cart.service";
+import { CartItem } from "app/restaurant-detail/shooping-cart/cart-item.model";
+
+@Injectable()
+export class OrderService{
+
+    constructor(private cartService: ShoopingCartService){}
+
+    cartItems(): CartItem[] {
+        return this.cartService.items
+    }
+
+    increaseQty(item: CartItem){
+        this.cartService.increaseQty(item)
+    }
+    
+    decreaseQty(item: CartItem){
+        this.cartService.decreaseQty(item)
+    }
+
+    remove(item: CartItem){
+        this.cartService.removeItem(item)
+    }
+
+}
