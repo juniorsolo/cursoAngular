c796224e65307f40ecaab323d88158de32762a56
Manoel <manoel.junior.ext@sascar.com>
2020-04-10 13:13:42 -0300

    Adicionando validator para mais de um campo.

--- meat-app-starter/src/app/order/order.component.ts
+++ meat-app-starter/src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shooping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -12,63 +12,82 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 })
 export class OrderComponent implements OnInit {
 
-  orderForm : FormGroup
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern = /^[0-9]*$/
+  orderForm: FormGroup
 
   delivery: number = 8
 
   paymentOptions: RadioOption[] = [
-    {label: 'Dinheiro', value: 'MON'},
-    {label: 'Cartão de Débito', value: 'DEB'},
-    {label: 'Cartão Refeição', value: 'REF'}
+    { label: 'Dinheiro', value: 'MON' },
+    { label: 'Cartão de Débito', value: 'DEB' },
+    { label: 'Cartão Refeição', value: 'REF' }
   ]
 
   constructor(private orderService: OrderService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.orderForm = new FormGroup({
-      name: new FormControl('', Validators.required),
-      email: new FormControl(''),
-      emailConfirmation: new FormControl(''),
-      address: new FormControl(''),
-      number: new FormControl(''),
-      optionalAddress: new FormControl(''),
-      paymentOption : new FormControl('')
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('', [Validators.required])
 
+    }, {
+      validator: OrderComponent.emailEqualsTo
     })
 
+  }
+  static emailEqualsTo(control: AbstractControl): { [key: string]: boolean }{
+
+      const email = control.get('email');
+      const emailConfirm = control.get('emailConfirmation');
+      if (!email || !emailConfirm) {
+        return undefined
+      }
+      if (email.value !== emailConfirm.value) {
+        return { emailNotMach: true }
+      }
+      
+      return undefined
+   
   }
 
   itemsValue(): number {
     return this.orderService.itemsValue()
   }
 
-  cartItems(): CartItem[]{
+  cartItems(): CartItem[] {
     return this.orderService.cartItems()
   }
 
-  increaseQty(item: CartItem){
+  increaseQty(item: CartItem) {
     this.orderService.increaseQty(item)
 
   }
-  decreaseQty(item: CartItem){
+  decreaseQty(item: CartItem) {
     this.orderService.decreaseQty(item)
   }
 
-  remove(item: CartItem){
+  remove(item: CartItem) {
     this.orderService.remove(item)
   }
 
-  checkOrder(order: Order){
+  checkOrder(order: Order) {
     this.orderForm.value
     order.orderItems = this.cartItems().
-    map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+      map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order).subscribe((orderId: string) => {
       this.router.navigate(['/order-summary']) // Redirecionando...
       console.log(`Compra concluída: ${orderId}`)
       //console.log(`Compra concluída: `+ JSON.stringify(orderId)) // Usado em caso de retornar um json
       this.orderService.clear()
     })
-    console.log( order)
+    console.log(order)
   }
 }
