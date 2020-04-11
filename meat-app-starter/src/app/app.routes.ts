e214a0e7c6075984735a23d7b775f84f6fb465d2
Manoel <manoel.junior.ext@sascar.com>
2020-04-10 23:03:43 -0300

    Carregamento tardio(lazy) do módulo about.

--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -10,7 +10,7 @@ import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 
 export const ROUTES: Routes =[
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: 'restaurants', component: RestaurantsComponent},
     {path: 'restaurants/:id', component: RestaurantDetailComponent,
         children:[
