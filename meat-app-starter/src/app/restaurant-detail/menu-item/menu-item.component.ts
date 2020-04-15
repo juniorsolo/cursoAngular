f7dd82ca407e73871528a5427c149da80844d389
Manoel <manoel.junior.ext@sascar.com>
2020-04-15 14:49:15 -0300

    Animando o menu-item.

--- meat-app-starter/src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ meat-app-starter/src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Menuitem } from './menu-item.model';
 import { TitleCasePipe } from '@angular/common';
+import { trigger, state, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready',[
+        style({opacity:0, transform: 'translateY(-20px)'}),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = 'ready'
 
   @Input() menuItem: Menuitem
   @Output() add = new EventEmitter()
